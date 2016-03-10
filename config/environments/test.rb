6d986d5549a9a37fc7798f504ae61ffed389b0aa
Ariana Garcia <arigarcia.978@gmail.com>
2016-03-10 15:20:50 -0300

    Problemas con heroku

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
+
   config.assets.initialize_on_precompile = false
 
   # Show full error reports and disable caching.
